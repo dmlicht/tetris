1557b6fa97d1c6a27149a714f184b4db5b141c3f
David Lichtenberg <david.m.lichtenberg@gmail.com>
2017-03-29 17:27:20 -0700

    slight cleanups

--- tetris.py
+++ tetris.py
@@ -10,8 +10,9 @@ BLOCK_DIMENSION = 4
 
 
 class Block():
+    """ Keeps track of offset positions, location and rotations of block """
     def __init__(self, square_offsets, position=(0, 0)):
-        self.block = np.zeros((BLOCK_DIMENSION, BLOCK_DIMENSION))  # todo remove
+        self.block = np.zeros((BLOCK_DIMENSION, BLOCK_DIMENSION))
         self.position = position
         self.square_offsets = square_offsets
         self.render_matrix()
@@ -44,82 +45,65 @@ class Board():
         if dimensions is None:
             dimensions = {"x": 10, "y": 10}
         if board is None:
-            self.board = np.zeros(dimensions.values())
+            self._board = np.zeros(dimensions.values())
         else:
-            self.board = board
+            self._board = board
 
     def anchor(self, block):
-        self.board = self._add_block_to_board(block)
+        self._board = self._add_block_to_board(block)
 
-    def show_block(self, block):
+    def preview_with_block(self, block):
         new_board = self._add_block_to_board(block)
         return Board(board=new_board)
 
     def _add_block_to_board(self, block):
-        new_board = np.copy(self.board)
+        new_board = np.copy(self._board)
         for location in block.get_absolute_locations():
             new_board[location[0], location[1]] += 1
         return new_board
 
-    def in_bounds(self, block):
-        for location in block.get_absolute_locations():
-            if location[0] < 0 or location[0] >= self.board.shape[0]:
-                return False
-            if location[1] < 0 or location[1] >= self.board.shape[1]:
-                return False
-        return True
-
     def can_place_block(self, block_locations):
         for location in block_locations:
             for dim in range(2):
-                if location[dim] >= self.board.shape[dim] or location[dim] < 0:
+                if location[dim] >= self._board.shape[dim] or location[dim] < 0:
                     return False
-            if self.board[location[0], location[1]] > 0:
+            if self._board[location[0], location[1]] > 0:
                 return False
         return True
 
     def can_move_down(self, block):
         return self.can_move_offset(block, (1, 0))
 
-    def can_move_right(self, block):
-        return self.can_move_offset(block, (0, 1))
-
-    def can_move_left(self, block):
-        return self.can_move_offset(block, (0, -1))
-
     def can_move_offset(self, block, offset):
         for location in block.get_absolute_locations():
             offset_location = location[0] + offset[0], location[1] + offset[1]
             for dim in range(2):
-                if offset_location[dim] >= self.board.shape[dim] or offset_location < 0:
+                if offset_location[dim] >= self._board.shape[dim] or offset_location < 0:
                     return False
-            if self.board[offset_location[0], offset_location[1]] > 0:
+            if self._board[offset_location[0], offset_location[1]] > 0:
                 return False
         return True
 
     def clear(self):
-        points = 0
-        ii = self.board.shape[0] - 1
+        """ Clears full bottom rows and returns amount of rows cleared """
+        rows_cleared = 0
+        ii = self._board.shape[0] - 1
         while ii >= 0:
-            row_full = all(self.board[ii, :])
+            row_full = all(self._board[ii, :])
             if row_full:
-                points += 1
+                rows_cleared += 1
                 ii -= 1
             else:
                 break
 
-        self.remove_bottom_n_rows(points)
-        return points
+        self.remove_bottom_n_rows(rows_cleared)
+        return rows_cleared
 
     def remove_bottom_n_rows(self, n):
         if n == 0:
             return
-        self.board[-1 * n:, :] = 0
-        self.board = np.roll(self.board, n, axis=0)
-
-
-def out_of_bounds_amount(board_size, block_size, offset):
-    return board_size - (offset + block_size)
+        self._board[-1 * n:, :] = 0
+        self._board = np.roll(self._board, n, axis=0)
 
 
 def next_block():
@@ -142,12 +126,12 @@ class Game():
         self.points = 0
 
     def run(self):
-        t = threading.Timer(3.0, self.handle_down)
-        t.start()
+        # t = threading.Timer(3.0, self.handle_down)
+        # t.start()
         while not self.game_over:
-            active_board = self.board.show_block(self.active_block)
+            active_board = self.board.preview_with_block(self.active_block)
             print("score:", self.points)
-            draw_board(active_board.board)
+            draw_board(active_board._board)
             next_move = getinput()
             if next_move == "up":
                 if self.board.can_place_block(self.active_block.preview_rotate()):
@@ -159,7 +143,14 @@ class Game():
                 if self.board.can_place_block(self.active_block.preview_move(MOVES['right'])):
                     self.active_block.position = move(self.active_block.position, next_move)
             elif next_move == "down":
-                self.handle_down()
+                if self.board.can_move_down(self.active_block):
+                    self.active_block.position = move(self.active_block.position, "down")
+                else:
+                    self.board.anchor(self.active_block)
+                    self.points += self.board.clear()
+                    self.active_block = next_block()
+                    if not self.board.can_place_block(self.active_block.get_absolute_locations()):
+                        self.game_over = True
         print("Game over, you got " + str(self.points) + " points.")
 
     def handle_down(self):
